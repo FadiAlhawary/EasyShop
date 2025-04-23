a8d2b6105ceac86a5b5bf823853cf83eecf21f5f
Fadi <fadihawari190@gmail.com>
2025-04-23 23:45:45 +0300

    made Product Card edited product to the product card

--- lib/pages/Products.dart
+++ lib/pages/Products.dart
@@ -1,3 +1,4 @@
+import 'package:easyshop/widgets/Product_Card_Widget.dart';
 import 'package:flutter/material.dart';
 import 'ProductView.dart';
 import 'Category.dart';
@@ -220,101 +221,20 @@ class _ProductsState extends State<Products> {
               padding: const EdgeInsets.all(16),
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-                childAspectRatio: 0.7, // Slightly taller
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 9,
+                childAspectRatio: 0.6, // Slightly taller
               ),
               itemCount: filteredProducts.length,
               itemBuilder: (context, index) {
                 final product = filteredProducts[index];
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) => ProductView(product: product),
-                      ),
-                    );
-                  },
-                  child: Card(
-                    elevation: 8,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(top: 8.0),
-                          child: Hero(
-                            tag: product['name'],
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(16),
-                                topRight: Radius.circular(16),
-                              ),
-                              child: Image.asset(
-                                product['image'],
-                                height: 120, // Smaller image height
-                                width: double.infinity,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                product['name'],
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16,
-                                  color: Colors.black87,
-                                ),
-                              ),
-                              SizedBox(height: 4),
-                              Text(
-                                "\$${product['price']}",
-                                style: TextStyle(
-                                  color: Colors.green,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                              SizedBox(height: 4),
-                              Text(
-                                product['desc'],
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                  fontSize: 13,
-                                  color: Colors.black54,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.topRight,
-                          child: IconButton(
-                            icon: Icon(
-                              product['isInWishlist']
-                                  ? Icons.favorite
-                                  : Icons.favorite_border,
-                              color:
-                                  product['isInWishlist']
-                                      ? Colors.red
-                                      : Colors.grey,
-                            ),
-                            onPressed: () => toggleWishlist(index),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                return ProductCardWidget(
+                  tag: product['name'],
+                  imageURL: product['image'],
+                  productName: product['name'],
+                  productPrice: product['price'],
+                  description: product['desc'],
+                  inWishList: product['isInWishlist'], destination: ProductView(product: product),
                 );
               },
             ),
