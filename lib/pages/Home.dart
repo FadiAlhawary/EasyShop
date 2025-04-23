880b456c158dc598fd1e81cee4370610fc9daa2d
Fadi <fadihawari190@gmail.com>
2025-04-23 21:55:39 +0300

    Making some changes

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -9,40 +9,32 @@ import 'package:flutter/material.dart';
 import 'Cart.dart';
 import 'ProductView.dart';
 import 'WishList.dart';
- 
-  class Home extends StatelessWidget {
-    const Home({super.key});
-  
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              FilledButton(onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return Products();
-                },));
-              }, child: Text('Products')),
 
-              FilledButton(onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return Cart();
-                },));
-              }, child: Text('carts')),
+class Home extends StatelessWidget {
+  const Home({super.key});
 
-              FilledButton(onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return Wishlist();
-                },));
-              }, child: Text('WishList')),
 
-              FilledButton(onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return Order();
-                },));
-              }, child: Text('Order')),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Products();
+                    },
+                  ),
+                );
+              },
+              child: Text('Products'),
+            ),
+
 
               // FilledButton(onPressed: () {
               //   Navigator.push(context, MaterialPageRoute(builder: (context) {
@@ -50,20 +42,105 @@ import 'WishList.dart';
               //   },));
               // }, child: Text('Category')),
 
-              FilledButton(onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return Dashboard();
-                },));
-              }, child: Text('Dashboard')),
+            FilledButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Cart();
+                    },
+                  ),
+                );
+              },
+              child: Text('Cart'),
+            ),
 
 
-              FilledButton(onPressed: () {
+            FilledButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Wishlist();
+                    },
+                  ),
+                );
+              },
+              child: Text('WishList'),
+            ),
+
+            FilledButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Order();
+                    },
+                  ),
+                );
+              },
+              child: Text('Order'),
+            ),
+
+
+            FilledButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Category(
+                        Categories: [],
+                        selectedCategory: '',
+                        onCategorySelected: (String category) {},
+                      );
+                    },
+                  ),
+                );
+              },
+              child: Text('Category'),
+            ),
+
+            FilledButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Dashboard();
+                    },
+                  ),
+                );
+              },
+              child: Text('Dashboard'),
+            ),
+
+            FilledButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return ProductView(product: {});
+                    },
+                  ),
+                );
+              },
+              child: Text('Product View'),
+            ),
+
+            FilledButton(
+              onPressed: () {
                 FirebaseAuth.instance.signOut();
-              }, child: Text('Log out')),
-            ],
-          ),
+              },
+              child: Text('Log out'),
+            ),
+          ],
         ),
-      );
-    }
+      ),
+    );
   }
-  
+}
